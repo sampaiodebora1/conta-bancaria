b3fe44cb07d237a4c85aafa47caf7ea1724c93f5
sampaiodebora <deebyolifran2@gmail.com>
2024-01-02 03:20:00 -0300

    metodos bancarios

--- Conta.java
+++ Conta.java
@@ -1,8 +1,50 @@
 package conta.model;
 
-public class Conta {
+public abstract class Conta {
 	private int numero;
+	private int agencia;
 	private String nome;
-	private String cpf;
+	private float saldo;
 
+	public Conta(int numero, int agencia, String nome, float saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
 }
